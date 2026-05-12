42a7393863e2e66f9c43b860d52d4055878805f8
sherin <sherinzhen99@gmail.com>
2026-05-12 21:15:38 +0700

    Add: sql-default-values - submission-0

---
+++ SQL/sql-default-values/submission-0.sql
@@ -0,0 +1,25 @@
+create table videos (
+    id integer,
+    name text default 'Untitled',
+    is_published boolean default false
+    );
+
+
+
+
+
+
+
+-- Do not modify below this line --
+INSERT INTO videos (id, name, is_published) 
+VALUES (1, 'My Video', true),
+       (2, 'Another Video', false);
+
+INSERT INTO videos (id)
+VALUES (3),
+       (4);
+
+INSERT INTO videos (name)
+VALUES ('Video with no ID');
+
+SELECT * FROM videos;
