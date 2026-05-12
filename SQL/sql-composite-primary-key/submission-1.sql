d484935cc3a8a16db385600ca95b4ee92d7308b2
sherin <sherinzhen99@gmail.com>
2026-05-12 21:36:53 +0700

    Add: sql-composite-primary-key - submission-1

---
+++ SQL/sql-composite-primary-key/submission-1.sql
@@ -0,0 +1,39 @@
+create table orders (
+    order_id integer,
+    product_id integer,
+    quantity integer,
+    primary key (order_id, product_id)
+);
+
+
+
+
+
+
+-- Do not modify below this line --
+SELECT 
+    c.table_name,
+    c.column_name, 
+    c.data_type, 
+    CASE 
+        WHEN kcu.column_name IS NOT NULL THEN 
+            CASE 
+                WHEN COUNT(*) OVER (PARTITION BY kcu.constraint_name) > 1 THEN 'YES (Composite)'
+                ELSE 'YES'
+            END
+        ELSE 'NO'
+    END AS is_primary_key
+FROM 
+    information_schema.columns c
+LEFT JOIN 
+    information_schema.key_column_usage kcu
+    ON c.table_name = kcu.table_name 
+    AND c.column_name = kcu.column_name
+LEFT JOIN 
+    information_schema.table_constraints tc
+    ON kcu.constraint_name = tc.constraint_name
+    AND tc.constraint_type = 'PRIMARY KEY'
+WHERE 
+    c.table_name = 'orders'
+ORDER BY 
+    c.ordinal_position;
