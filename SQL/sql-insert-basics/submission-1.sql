9c2e1cac227c8d53f770ed6e498b889239db615b
sherin <sherinzhen99@gmail.com>
2026-05-13 14:24:22 +0700

    Add: sql-insert-basics - submission-1

---
+++ SQL/sql-insert-basics/submission-1.sql
@@ -0,0 +1,23 @@
+CREATE TYPE account_type AS ENUM ('checking', 'savings', 'cd', 'money_market');
+
+CREATE TABLE bank_accounts (
+    id INTEGER PRIMARY KEY,
+    account_type account_type,
+    balance INTEGER
+);
+-- Do not modify above this line --
+
+insert into bank_accounts (id, account_type, balance)
+    values (1, 'checking', 1000),
+    (2, 'savings', 2000),
+    (3, 'cd', 3000),
+    (4, 'money_market', 4000);
+
+
+
+
+
+
+
+-- Do not modify below this line --
+SELECT * FROM bank_accounts;
