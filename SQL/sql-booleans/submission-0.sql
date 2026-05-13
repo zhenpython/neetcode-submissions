642bad106c07c6a599f5aaeb4b99316e0ccd039e
sherin <sherinzhen99@gmail.com>
2026-05-13 13:46:09 +0700

    Add: sql-booleans - submission-0

---
+++ SQL/sql-booleans/submission-0.sql
@@ -0,0 +1,22 @@
+create table three_column_table (
+    id integer primary key,
+    is_active boolean,
+    is_admin boolean
+);
+
+
+
+
+
+
+-- Do not modify below this line --
+INSERT INTO three_column_table (id, is_active, is_admin) 
+  VALUES (1, TRUE, FALSE),
+         (2, true, false),
+         (3, 't', 'f'),
+         (4, 'y', 'n'),
+         (5, 'yes', 'no'),
+         (6, 'on', 'off'),
+         (7, '1', '0');
+
+SELECT * FROM three_column_table;
