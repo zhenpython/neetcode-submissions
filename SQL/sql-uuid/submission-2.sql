a331f3cf32dbc0f44e7f7e88899b049f4d37567f
sherin <sherinzhen99@gmail.com>
2026-05-13 14:20:47 +0700

    Add: sql-uuid - submission-2

---
+++ SQL/sql-uuid/submission-2.sql
@@ -0,0 +1,13 @@
+CREATE TABLE users (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    name TEXT
+);
+
+
+-- TODO: Remove this block --
+-- TODO: Remove this block --
+
+
+
+-- Do not modify below this line --
+SELECT * FROM users;
