23ae58148fa4ba062f7a707caf9b0bba8da1dc47
sherin <sherinzhen99@gmail.com>
2026-05-17 20:31:22 +0700

    Add: sql-select-in - submission-2

---
+++ SQL/sql-select-in/submission-2.sql
@@ -0,0 +1,28 @@
+CREATE TABLE cities (
+    id INT PRIMARY KEY,
+    name TEXT,
+    country TEXT
+);
+
+INSERT INTO cities (id, name, country) VALUES
+(1, 'Tokyo', 'Japan'),
+(2, 'London', 'England'),
+(3, 'Paris', 'France'),
+(4, 'New York', 'USA'),
+(5, 'New Delhi', 'India'),
+(6, 'Shanghai', 'China'),
+(7, 'Sao Paulo', 'Brazil'),
+(8, 'Mexico City', 'Mexico'),
+(9, 'Cairo', 'Egypt'),
+(10, 'Mumbai', 'India'),
+(11, 'Beijing', 'China'),
+(12, 'Los Angeles', 'USA'),
+(13, 'Toronto', 'Canada'),
+(14, 'Vancouver', 'Canada');
+-- Do not modify above this line. --
+
+
+
+select name from cities
+where country in ('USA', 'Canada', 'Mexico');
+
