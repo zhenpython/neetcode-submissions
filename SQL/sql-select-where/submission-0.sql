3f30ea2ea2cd0579d228e3fc802c9e8c8e79aa34
sherin <sherinzhen99@gmail.com>
2026-05-17 20:15:15 +0700

    Add: sql-select-where - submission-0

---
+++ SQL/sql-select-where/submission-0.sql
@@ -0,0 +1,22 @@
+CREATE TABLE athletes (
+    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    name TEXT,
+    sport TEXT
+);
+
+INSERT INTO athletes (name, sport) VALUES
+  ('LeBron James', 'Basketball'),
+  ('Serena Williams', 'Tennis'),
+  ('Michael Jordan', 'Basketball'),
+  ('Kobe Bryant', 'Basketball'),
+  ('Cristiano Ronaldo', 'Soccer'),
+  ('Lionel Messi', 'Soccer'),
+  ('Tom Brady', 'Football'),
+  ('Usain Bolt', 'Track and Field');
+-- Do not modify above this line. --
+
+select name, sport from athletes
+where sport = 'Basketball';
+
+
+
